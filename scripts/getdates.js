9c9ec630af881e5dd8333ee0064c2d4adaa17056
EduardoSanabria-01 <vsanabriadiaz@byupathway.edu>
2026-07-04 15:58:12 +0200

    1project

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// Dynamically populate the current year in the footer
+const currentYearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+currentYearSpan.textContent = currentYear;
+
+// Dynamically populate the last modified date in the footer
+const lastModifiedParagraph = document.getElementById("lastModified");
+lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
